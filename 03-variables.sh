ccb22f0acedddea7e29a6c5533424a4ce756c69a
Dheeraja <mftodevops@gmail.com>
2025-06-13 00:34:49 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 PERSON1=Ramesh
 PERSOn2=Suresh
 
-echo"$PERSON1::Hi $PERSON2"
-echo"$PERSON2::Hello $PERSON1"
-echo"$PERSON1::How are you doing?"
-echo"$PERSON2::I am good,how are you?"
+echo "$PERSON1::Hi $PERSON2"
+echo "$PERSON2::Hello $PERSON1"
+echo "$PERSON1::How are you doing?"
+echo "$PERSON2::I am good,how are you?"
