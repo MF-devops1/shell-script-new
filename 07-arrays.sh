340ca42cdfad4152680f1e9b484ab3bcfa800b0e
Dheeraja <mftodevops@gmail.com>
2025-06-13 08:48:20 +0530

    variables

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+MOVIES=("Pushpa" "RRR" "DEVARA")
+
+echo "first move is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"
+
+echo "All movies are :${MOVIES[@]}"
