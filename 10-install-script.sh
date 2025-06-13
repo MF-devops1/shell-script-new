b62237f03ee9f96825fe35f3c1b74f018041e148
Dheeraja <mftodevops@gmail.com>
2025-06-13 22:46:49 +0530

    variables

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,54 +5,55 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR: you must have sudo access to execute this script"
-    exit 1
+    # exit 1
 fi
-dnf list installed mysql
+dnf install mysqll -y
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-dnf install mysql -y
-if [ $? -ne 0 ]
-then
-echo "Installing mysql .... FAILURE"
-exit 1
-else
-echo "Installing MYSQL ....SUCCESS"
-    fi
-else
-    echo "MySQL is already ... INSTALLED"
-fi
+# if [ $? -ne 0 ]
+# then 
 
 # if [ $? -ne 0 ]
 # then
-#     echo "Installing MySQL ... FAILURE"
-#     exit 1
+# echo "Installing mysql .... FAILURE"
+# exit 1
 # else
-#     echo "Installing MySQL ... SUCCESS"
+# echo "Installing MYSQL ....SUCCESS"
+#     fi
+# else
+#     echo "MySQL is already ... INSTALLED"
 # fi
 
-dnf list installed git
+# # if [ $? -ne 0 ]
+# # then
+# #     echo "Installing MySQL ... FAILURE"
+# #     exit 1
+# # else
+# #     echo "Installing MySQL ... SUCCESS"
+# # fi
 
-if [ $? -ne 0 ]
-then
-    dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "Installing Git ... FAILURE"
-        exit 1
-    else
-        echo "Installing Git ... SUCCESS"
-    fi
-else
-    echo "Git is already ... INSTALLED"
-fi
-
+# dnf list installed git
 
 # if [ $? -ne 0 ]
 # then
-#     echo "Installing Git ... FAILURE"
-#     exit 1
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Installing Git ... FAILURE"
+#         exit 1
+#     else
+#         echo "Installing Git ... SUCCESS"
+#     fi
 # else
-#     echo "Installing Git ... SUCCESS"
+#     echo "Git is already ... INSTALLED"
 # fi
+
+
+# # if [ $? -ne 0 ]
+# # then
+# #     echo "Installing Git ... FAILURE"
+# #     exit 1
+# # else
+# #     echo "Installing Git ... SUCCESS"
+# # fi
 
