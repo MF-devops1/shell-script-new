12530f104cc869446f1bc46cc6ef281d3559730c
Dheeraja <mftodevops@gmail.com>
2025-06-13 12:10:18 +0530

    variables

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,8 +5,54 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR: you must have sudo access to execute this script"
-    # exit 1
+    exit 1
 fi
-dnf install mysqll -y
+dnf list installed mysql
 
-dnf install git -y
+if [$? -ne 0 ]
+then 
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+echo "Installing mysql .... FAILURE"
+exit 1
+else
+echo "Installing MYSQL ....SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git is already ... INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
+
