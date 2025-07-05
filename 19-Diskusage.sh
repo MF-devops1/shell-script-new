75784ddecf3580879c59ee79444b76e6cb29ef82
Dheeraja <mftodevops@gmail.com>
2025-07-05 08:08:14 +0530

    programs added

--- 19-Diskusage.sh
+++ 19-Diskusage.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-# DISK_THRESHOLD=5 #real projects will monitor for 70
+DISK_THRESHOLD=5 #real projects will monitor for 70
 # MSG=""
 
 while read -r line
 do
-    echo $line
-    # USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    # PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    # #echo "Partition: $PARTITION , Usage: $USAGE"
+    USAGE=$(echo $line | awk -F " " '{print $6F}' )
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION , Usage: $USAGE"
     # if [ $USAGE -ge $DISK_THRESHOLD ]
     # then
     #     MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
